5d8afea7839ea18bfdd19814e04fefb768c99ee4
alihamza-doc <alihamza141250@gmail.com>
2025-07-15 23:31:27 -0700

    projectsAdded

--- frontend/src/Pages/Projects.jsx
+++ frontend/src/Pages/Projects.jsx
@@ -5,11 +5,26 @@ export default function project() {
     <>
     <h1 className="text-center text-blue-600 text-3xl p-3">My Projects</h1>
     <p  className=" text-cyan-600 text-2xl p-4 ">My E-commerce Website</p>
-    <ol>
-    <li>
+    
+    
     <a  className="p-4 text-black underline text-2xl" href="https://e-commerce-five-sand-35.vercel.app/"><FontAwesomeIcon icon={faArrowRight} /><span>  </span>  E-Shop</a>
-    </li>
-    </ol>
+    
+    <p  className=" text-cyan-600 text-2xl p-4 ">Age Calculator</p>
+    
+    
+    <a  className="p-4 text-black underline text-2xl" href="https://age-calculator-tau-jet.vercel.app/"><FontAwesomeIcon icon={faArrowRight} /><span>  </span>  AgeCalculator</a>
+    
+    <p  className=" text-cyan-600 text-2xl p-4 ">Text-to-voice Converter</p>
+    
+    
+    <a  className="p-4 text-black underline text-2xl" href="https://text-to-voice-six-lime.vercel.app/"><FontAwesomeIcon icon={faArrowRight} /><span>  </span>  TextToVoiceConverter</a>
+    
+    <p  className=" text-cyan-600 text-2xl p-4 ">Quiz App</p>
+    
+    
+    <a  className="p-4 text-black underline text-2xl pb-4" href="https://quiz-app-teal-mu-70.vercel.app/"><FontAwesomeIcon icon={faArrowRight} /><span>  </span>  QuizApp</a>
+    
+    
     </> 
   )
 }
